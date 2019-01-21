12bfc6a63542f4520ad2007baa6c86652ec847ff
rushilmatta15 <rushil18csu184@ncuindia.edu>
2019-01-21 11:26:01 +0530

    third of exercise

---
+++ javap/data ops/thirdd.java
@@ -0,0 +1,11 @@
+class thirdd
+{
+	public static void main(String args[])
+	{
+    System.out.println(7<=5);
+	System.out.println(true==true);
+	System.out.println(7==4);
+	System.out.println(true&&false);
+	System.out.println(true||false);	
+}
+}
