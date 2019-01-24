a6893634f4196993352d5918347109ecb80c6340
rushilmatta15 <rushil18csu184@ncuindia.edu>
2019-01-24 11:58:31 +0530

    array ex

---
+++ javap/array/2darr.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class first
+{
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		int i,j;
+		int arr[][]= new int[5][5];
+		System.out.println("Enter upto two digit nos.");
+		for(i=0;i<5;i++)
+		{
+			for(j=0;j<5;j++)
+			{
+				do
+				{
+					arr[i][j]= scan.nextInt();
+				}
+				while(arr[i][j]>999);
+			}
+		}
+		for(i=0;i<5;i++)
+		{
+			for(j=0;j<5;j++)
+			{
+				System.out.print(arr[i][j]);
+				if(arr[i][j]%5==0)
+				{
+					continue;
+				}
+				else
+				{
+					arr[i][j]=arr[i][j]*2;
+				}
+			}
+			System.out.println();
+		}
+        System.out.println("Double of odd");
+		for(i=0;i<5;i++)
+		{
+			for(j=0;j<5;j++)
+			{
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+		
+
+	}
+}
